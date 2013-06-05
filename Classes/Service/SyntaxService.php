c16e354764f166076c9a53027f78c16bf4338d24
Claus Due <claus@wildside.dk>
2013-06-06 00:45:04 +0200

    [FEATURE] Add SyntaxService to check various files' syntax

---
+++ Classes/Service/SyntaxService.php
@@ -0,0 +1,62 @@
+<?php
+
+class Tx_Builder_Service_SyntaxService implements t3lib_Singleton {
+
+	/**
+	 * @var Tx_Extbase_Object_ObjectManagerInterface
+	 */
+	protected $objectManager;
+
+	/**
+	 * @var Tx_Fluid_Core_Parser_TemplateParser
+	 */
+	protected $templateParser;
+
+	/**
+	 * @param Tx_Extbase_Object_ObjectManagerInterface $objectManager
+	 * @return void
+	 */
+	public function injectObjectManager(Tx_Extbase_Object_ObjectManagerInterface $objectManager) {
+		$this->objectManager = $objectManager;
+	}
+
+	/**
+	 * @param Tx_Fluid_Core_Parser_TemplateParser $templateParser
+	 * @return void
+	 */
+	public function injectTemplateParser(Tx_Fluid_Core_Parser_TemplateParser $templateParser) {
+		$this->templateParser = $templateParser;
+	}
+
+	/**
+	 * Syntax checks a Fluid template file by attempting
+	 * to load the file and retrieve a parsed template, which
+	 * will cause traversal of the entire syntax node tree
+	 * and report any errors about missing or unknown arguments.
+	 *
+	 * Will NOT, however, report errors which are caused by
+	 * variables assigned to the template (there will be no
+	 * variables while building the syntax tree and listening
+	 * for errors).
+	 *
+	 * @param string $filePathAndFilename
+	 * @return Tx_Builder_Result_FluidParserResult
+	 */
+	public function syntaxCheckFluidTemplateFile($filePathAndFilename) {
+		/** @var $result Tx_Builder_Result_FluidParserResult */
+		$result = $this->objectManager->get('Tx_Builder_Result_FluidParserResult');
+		/** @var $context Tx_Fluid_Core_Rendering_RenderingContext */
+		$context = $this->objectManager->get('Tx_Fluid_Core_Rendering_RenderingContext');
+		try {
+			$parsedTemplate = $this->templateParser->parse(file_get_contents($filePathAndFilename));
+			$result->setLayoutName($parsedTemplate->getLayoutName($context));
+			$result->setNamespaces($this->templateParser->getNamespaces());
+			$result->setCompilable($parsedTemplate->isCompilable());
+		} catch (Exception $error) {
+			$result->setError($error);
+			$result->setValid(FALSE);
+		}
+		return $result;
+	}
+
+}
