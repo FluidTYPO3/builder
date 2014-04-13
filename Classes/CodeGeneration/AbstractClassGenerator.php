0efedbeb077fd75cef0f3c131df0413c4a09c3d2
G. Bodenschatz <coding@46halbe.de>
2014-04-13 21:53:51 +0200

    [TASK] Fix namespace issues in CodeGeneration
    * normalize namespace usage (code + annotations)
    * remove unused use statements
    * update copyright info

--- Classes/CodeGeneration/AbstractClassGenerator.php
+++ Classes/CodeGeneration/AbstractClassGenerator.php
@@ -1,7 +1,30 @@
 <?php
 namespace FluidTYPO3\Builder\CodeGeneration;
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2014 Claus Due <claus@namelesscoder.net>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ * ************************************************************* */
 
 use FluidTYPO3\Builder\Service\ClassAnalysisService;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 abstract class AbstractClassGenerator extends AbstractCodeGenerator implements ClassGeneratorInterface {
 
@@ -125,7 +148,7 @@ abstract class AbstractClassGenerator extends AbstractCodeGenerator implements C
 			}
 		}
 		if (TRUE === $shouldBeWritten) {
-			t3lib_div::writeFile($filePathAndFilename, $code);
+			GeneralUtility::writeFile($filePathAndFilename, $code);
 		}
 	}
 
@@ -140,7 +163,8 @@ abstract class AbstractClassGenerator extends AbstractCodeGenerator implements C
 		}
 		$properties = array_map('trim', $this->properties);
 		$methods = array_map('trim', $this->methods);
-		$this->appendCommonTestMethods();
+        // @todo check if needed
+		// $this->appendCommonTestMethods();
 		$variables = array(
 			'class' => $className,
 			'author' => $this->author,
