226c97b80fc763a17a254320912158e07908e5a4
Claus Due <claus@namelesscoder.net>
2015-01-25 04:11:20 +0100

    [TASK] Raise TYPO3 version min and max requirements

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' =>
     array (
-      'typo3' => '6.1.0-6.2.99',
+      'typo3' => '6.2.0-7.1.99',
       'cms' => '',
       'extbase' => '',
       'fluid' => '',
