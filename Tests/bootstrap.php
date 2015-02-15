296f4090e351e41b27256c2012054bb47608401f
Claus Due <claus@namelesscoder.net>
2015-02-15 18:39:51 +0100

    [TASK] Update dependencies

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -9,38 +9,13 @@ if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
 /** @var Composer\Autoload\ClassLoader $autoloader */
 $autoloader = require __DIR__ . '/../vendor/autoload.php';
 $autoloader->addPsr4('FluidTYPO3\\Builder\\Tests\\Fixtures\\', __DIR__ . '/Fixtures/');
+$autoloader->addPsr4('FluidTYPO3\\Builder\\Tests\\Unit\\', __DIR__ . '/Unit/');
 
-define('PATH_thisScript', realpath('vendor/typo3/cms/typo3/index.php'));
-define('TYPO3_MODE', 'BE');
-putenv('TYPO3_CONTEXT=Testing');
-
-$nullCache = array(
-	'frontend' => 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend',
-	'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\NullBackend'
-);
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'] = array(
-	'extbase_typo3dbbackend_queries' => $nullCache,
-	'extbase_typo3dbbackend_tablecolumns' => $nullCache,
-	'extbase_datamapfactory_datamap' => $nullCache,
-	'extbase_object' => $nullCache,
-	'extbase_reflection' => $nullCache
-);
-
-\TYPO3\CMS\Core\Core\Bootstrap::getInstance()
-	->baseSetup('typo3/')
-	->initializeClassLoader()
-
-	->initializeCachingFramework();
-/** @var $extbaseObjectContainer \TYPO3\CMS\Extbase\Object\Container\Container */
-$extbaseObjectContainer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Object\\Container\\Container');
-$extbaseObjectContainer->registerImplementation('TYPO3\\CMS\\Extbase\\Configuration\\ConfigurationManagerInterface', 'FluidTYPO3\\Builder\\Tests\\Fixtures\\Classes\\DummyConfigurationManager');
-$extbaseObjectContainer->registerImplementation(
-	'TYPO3\\CMS\\Extbase\\Persistence\\PersistenceManagerInterface',
-	'FluidTYPO3\\Builder\\Tests\\Fixtures\\Classes\\DummyPersistenceManager'
-);
-$extbaseObjectContainer->registerImplementation(
-	'TYPO3\\CMS\\Extbase\\Persistence\\Generic\\BackendInterface',
-	'FluidTYPO3\\Builder\\Tests\\Fixtures\\Classes\\DummyPersistenceBackend'
-);
-
-unset($extbaseObjectContainer);
+\FluidTYPO3\Development\Bootstrap::initialize(array(
+	'cache_core' => \FluidTYPO3\Development\Bootstrap::CACHE_PHP_NULL,
+	'extbase_typo3dbbackend_queries' => \FluidTYPO3\Development\Bootstrap::CACHE_NULL,
+	'extbase_typo3dbbackend_tablecolumns' => \FluidTYPO3\Development\Bootstrap::CACHE_NULL,
+	'extbase_datamapfactory_datamap' => \FluidTYPO3\Development\Bootstrap::CACHE_NULL,
+	'extbase_object' => \FluidTYPO3\Development\Bootstrap::CACHE_NULL,
+	'extbase_reflection' => \FluidTYPO3\Development\Bootstrap::CACHE_NULL
+));
