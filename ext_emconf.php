1e010c372ac6b02e7e92aa50530e39b643979a83
Claus Due <claus@namelesscoder.net>
2015-08-08 03:19:18 +0200

    [TASK] Raise maximum supported TYPO3 core version to 7.4.99

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   'author_company' => '',
   'constraints' => array (
     'depends' => array (
-      'typo3' => '6.2.0-7.1.99',
+      'typo3' => '6.2.0-7.4.99',
       'cms' => '',
       'vhs' => '2.2.0-2.3.99',
     ),
