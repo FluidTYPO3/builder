7b0370a9aa4241c56b9592a6146c608b28768fde
Claus Due <claus@namelesscoder.net>
2015-11-07 18:48:49 +0100

    [TASK] Raise required PHP version, remove "cms" dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,8 +21,8 @@ $EM_CONF[$_EXTKEY] = array (
   'author_company' => '',
   'constraints' => array (
     'depends' => array (
+      'php' => '5.5.0-5.6.99',
       'typo3' => '6.2.0-7.4.99',
-      'cms' => '',
       'vhs' => '2.2.0-2.3.99',
     ),
     'conflicts' => array (
