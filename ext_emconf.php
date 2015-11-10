61ee8a27db5c9dcd100823fb542995f35db64fad
Claus Due <claus@namelesscoder.net>
2015-11-10 15:54:57 +0100

    [TASK] Raise min/max required TYPO3 and VHS versions

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,8 +22,8 @@ $EM_CONF[$_EXTKEY] = array (
   'constraints' => array (
     'depends' => array (
       'php' => '5.5.0-5.6.99',
-      'typo3' => '6.2.0-7.6.99',
-      'vhs' => '2.2.0-2.3.99',
+      'typo3' => '7.4.0-7.6.99',
+      'vhs' => '2.2.0-2.4.99',
     ),
     'conflicts' => array (
     ),
