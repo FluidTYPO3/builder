58ad2d5c0bdccda85ff44cc23afeaed28d7b65af
Claus Due <claus@namelesscoder.net>
2016-06-06 19:48:10 +0200

    [TASK] Raise maximum PHP version

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   'author_company' => '',
   'constraints' => array (
     'depends' => array (
-      'php' => '5.5.0-5.6.99',
+      'php' => '5.5.0-7.0.99',
       'typo3' => '7.4.0-8.1.99',
       'vhs' => '2.2.0-2.4.99',
     ),
