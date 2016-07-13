b23d46f4e9eedb32da82c0378bab000b59d67b2c
Claus Due <claus@namelesscoder.net>
2016-07-13 14:14:16 +0200

    [TASK] Raise maximum supported TYPO3 version

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,8 +22,8 @@ $EM_CONF[$_EXTKEY] = array (
   'constraints' => array (
     'depends' => array (
       'php' => '5.5.0-7.0.99',
-      'typo3' => '7.4.0-8.1.99',
-      'vhs' => '2.2.0-2.4.99',
+      'typo3' => '7.4.0-8.2.99',
+      'vhs' => '2.2.0-3.99.99',
     ),
     'conflicts' => array (
     ),
