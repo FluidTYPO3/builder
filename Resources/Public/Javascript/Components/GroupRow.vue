35c76a12bb44a996956881f780066353175f60c7
Marc Neuhaus <marc@mia3.com>
2017-01-30 21:56:54 +0100

    [TASK] cleanup

---
+++ Resources/Public/Javascript/Components/GroupRow.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div class="group-row">
+            <div class="group-row-label">
+                <label>Name:</label>
+            </div>
+            <div class="group-row-content">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'group-row',
+        props: ['label', 'description']
+    }
+</script>
