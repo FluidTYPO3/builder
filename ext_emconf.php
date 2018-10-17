728ddb31ee31ddf548c7b2d39b035be71c04097a
Claus Due <claus@namelesscoder.net>
2018-10-18 01:46:52 +0200

    [REMOVAL] Raise minimum PHP requirement to 7.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   'author_company' => '',
   'constraints' => array (
     'depends' => array (
-      'php' => '5.5.0-7.1.99',
+      'php' => '7.0.0-7.2.99',
       'typo3' => '8.7.0-9.5.99',
       'vhs' => '2.2.0-4.99.99',
     ),
