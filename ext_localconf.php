9752f23fc71a1369cca973a60d8c61f7a00ec619
Claus Due <claus@namelesscoder.net>
2018-10-18 02:04:06 +0200

    [FEATURE] Provide extension configuration toggles
    Provides toggles for the frontend plugins and makes
    their use opt-in and disabled by default.

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,28 +1,43 @@
 <?php
+
+use FluidTYPO3\Builder\Command\BuilderCommandController;
+
 if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin('FluidTYPO3.Builder', 'Frontend', array('Frontend' => 'build'), array('Frontend' => 'build'));
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin('FluidTYPO3.Builder', 'Doodle', array('Frontend' => 'doodle,renderFluid'), array('Frontend' => 'renderFluid'));
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin('FluidTYPO3.Builder', 'Render', array('Frontend' => 'renderFluid'), array('Frontend' => 'renderFluid'));
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('
-[GLOBAL]
-builderDoodle = PAGE
-builderDoodle {
-	typeNum = 9967
-	config {
-		no_cache = 1
-		disableAllHeaderCode = 1
-	}
-	9967 = USER
-	9967 {
-		userFunc = TYPO3\CMS\Extbase\Core\Bootstrap->run
-		vendorName = FluidTYPO3
-		extensionName = Builder
-		pluginName = Render
-	}
+if (!(TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_INSTALL)) {
+
+    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['builder']['setup'] = unserialize($_EXTCONF);
+
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = BuilderCommandController::class;
+
+    if (!empty($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['builder']['setup']['enableDoodlePlugin'])) {
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin('FluidTYPO3.Builder', 'Doodle', array('Frontend' => 'doodle,renderFluid'), array('Frontend' => 'renderFluid'));
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin('FluidTYPO3.Builder', 'Render', array('Frontend' => 'renderFluid'), array('Frontend' => 'renderFluid'));
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('
+        [GLOBAL]
+        builderDoodle = PAGE
+        builderDoodle {
+            typeNum = 9967
+            config {
+                no_cache = 1
+                disableAllHeaderCode = 1
+            }
+            9967 = USER
+            9967 {
+                userFunc = TYPO3\CMS\Extbase\Core\Bootstrap->run
+                vendorName = FluidTYPO3
+                extensionName = Builder
+                pluginName = Render
+            }
+        }
+        [GLOBAL]
+        ');
+    }
+    if (!empty($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['builder']['setup']['enableFrontendPlugin'])) {
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin('FluidTYPO3.Builder', 'Frontend', array('Frontend' => 'build'), array('Frontend' => 'build'));
+    }
 }
-[GLOBAL]
-');
