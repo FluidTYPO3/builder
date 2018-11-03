552e36d521361e3a9fef74dd6b8d814a904fdaea
Claus Due <claus@namelesscoder.net>
2018-11-04 00:15:31 +0100

    [BUGFIX] Move BE module registration to ext_tables.php

---
+++ ext_tables.php
@@ -0,0 +1,32 @@
+<?php
+
+if (!defined('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+if (!(TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_INSTALL)) {
+    (function($config) {
+        if ('BE' === TYPO3_MODE) {
+            \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+                'FluidTYPO3.Builder',
+                'tools',
+                'txbuilderM1',
+                '',
+                [
+                    'Backend' => 'index,syntax,build,buildForm',
+                ],
+                [
+                    'access' => 'user,group',
+                    'icon' => 'EXT:builder/Resources/Public/Icons/module_builder.png',
+                    'labels' => 'LLL:EXT:builder/Resources/Private/Language/locallang.xlf'
+                ]
+            );
+
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+                'builder',
+                'EXT:builder/Resources/Private/Language/locallang.xlf'
+            );
+        }
+
+    })($_EXTCONF);
+}
