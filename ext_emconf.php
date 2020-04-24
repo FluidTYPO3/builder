fd248859441fa2a12bd3260ee278b57ef5199d7b
Claus Due <claus@namelesscoder.net>
2020-04-24 16:45:48 +0200

    [TASK] Declare compatibility with TYPO3 v10 branches
    And declare compatibility with PHP up to 7.4.

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,9 +21,9 @@ $EM_CONF[$_EXTKEY] = array (
   'author_company' => '',
   'constraints' => array (
     'depends' => array (
-      'php' => '7.0.0-7.2.99',
-      'typo3' => '8.7.0-9.5.99',
-      'vhs' => '2.2.0-5.99.99',
+      'php' => '7.0.0-7.4.99',
+      'typo3' => '8.7.0-10.4.99',
+      'vhs' => '2.2.0-6.99.99',
     ),
     'conflicts' => array (
     ),
